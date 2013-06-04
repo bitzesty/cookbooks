f318ba962963528d2ff86c55bc6b54b86d493a87
Saulius Grigaliunas <saulius@ninja.lt>
2013-06-04 17:04:45 +0300

    rbenv attributes are dependent on user so include them first

--- bz-app/attributes/rbenv.rb
+++ bz-app/attributes/rbenv.rb
@@ -1,3 +1,5 @@
+include_attribute "bz-app::user"
+
 default['bz-app']['rbenv']['path'] = "/home/#{node['bz-app']['user']['name']}/.rbenv"
 default['bz-app']['rbenv']['plugins'] = ['https://github.com/sstephenson/ruby-build.git']
 default['bz-app']['rbenv']['rubies']  = ['2.0.0-p0']
