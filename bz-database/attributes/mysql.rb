62dfa8b83d4a950ac953d79381f902250a61f775
Saulius Grigaliunas <saulius@ninja.lt>
2013-06-05 10:27:33 +0300

    Extend mysql recipe

---
+++ bz-database/attributes/mysql.rb
@@ -0,0 +1,13 @@
+include_attribute "bz-server::app"
+
+default['bz-database']['mysql']['database_name'] = node['bz-server']['app']['name']
+default['bz-database']['mysql']['database_host'] = 'localhost'
+default['bz-database']['mysql']['database_username'] = node['bz-server']['app']['name']
+default['bz-database']['mysql']['database_password'] = 'thisissomerandompassword1'
+
+# general info
+
+default['bz-database']['mysql']['root_user_name'] = 'root'
+default['bz-database']['mysql']['root_password'] = 'thisissomerandompassword1'
+default['bz-database']['mysql']['server_repl_password'] = 'thisissomerandompassword2'
+default['bz-database']['mysql']['server_debian_password'] = 'thisissomerandompassword3'
---
+++ bz-database/recipes/mysql.rb
@@ -0,0 +1,33 @@
+include_recipe "database"
+include_recipe "database::mysql"
+
+node['mysql']['remove_anonymous_users'] = true # Remove anonymous users
+node['mysql']['allow_remote_root'] = false # Root can only login from localhost
+node['mysql']['remove_test_database'] = true
+
+node['mysql']['server_root_password'] = node['bz-database']['mysql']['root_password'] # Set the server's root password
+node['mysql']['server_repl_password'] = node['bz-database']['mysql']['server_repl_password'] # Set the replication user 'repl' password
+node['mysql']['server_debian_password'] = node['bz-database']['mysql']['server_debian_password'] # Set the debian-sys-maint user password
+
+include_recipe "mysql::server"
+include_recipe "mysql::client"
+
+mysql_connection_info = {
+  :host => node['bz-database']['mysql']['database_host'],
+  :username => node['bz-database']['mysql']['root_user_name'],
+  :password => node['bz-database']['mysql']['root_password']
+}
+
+# create a mysql database
+mysql_database node['bz-database']['mysql']['database_name'] do
+  connection mysql_connection_info
+
+  action :create
+end
+
+mysql_database_user node['bz-database']['mysql']['database_username'] do
+  connection mysql_connection_info
+
+  password node['bz-database']['mysql']['database_password']
+  action :create
+end
