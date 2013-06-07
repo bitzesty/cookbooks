f798813f5c5c8177a36fa7457f08f45903fb6403
Saulius Grigaliunas <saulius@ninja.lt>
2013-06-07 11:03:24 +0300

    Revert "Create current directory for deploy"
    This reverts commit c4b2a0e8c6ddbfc28b0bc588b49626b891d04261.
    
    Is created by capistrano by symlinking

--- bz-rails/attributes/default.rb
+++ bz-rails/attributes/default.rb
@@ -4,6 +4,5 @@ include_attribute "bz-server::user"
 default['bz-rails']['environment'] = nil
 default['bz-rails']['shared_path'] = "#{node['bz-server']['app']['path']}/shared"
 default['bz-rails']['releases_path'] = "#{node['bz-server']['app']['path']}/releases"
-default['bz-rails']['current_path'] = "#{node['bz-server']['app']['path']}/current"
 default['bz-rails']['_default_shared_directories'] = ['config', 'system', 'log', 'pids', 'sockets']
 default['bz-rails']['shared_directories'] = []
