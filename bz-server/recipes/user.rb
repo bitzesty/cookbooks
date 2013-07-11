46fcc1589a027e643cf9e5a3eac40281325508dc
Saulius Grigaliunas <saulius@ninja.lt>
2013-07-11 18:44:57 +0300

    Wrong authorized_keys format

--- bz-server/recipes/user.rb
+++ bz-server/recipes/user.rb
@@ -66,7 +66,7 @@ unless node['bz-server']['user']['authorized_github_users'].empty?
     user_keys = open("https://github.com/#{github_user}.keys").read
     user_keys.split("\n").each_with_index { |key, index|
       # NOTE uses Hashie::Mash
-      node['bz-server']['user']['authorized_keys'].send("#{github_user}_github_#{index}=", key.strip.chomp('=='))
+      node['bz-server']['user']['authorized_keys'].send("#{github_user}_github_#{index}=", key.strip)
     }
   end
 end
@@ -75,7 +75,7 @@ file "/home/#{node['bz-server']['user']['name']}/.ssh/authorized_keys" do
   owner node['bz-server']['user']['name']
   group node['bz-server']['user']['name']
   mode "600"
-  content node['bz-server']['user']['_default_authorized_keys'].to_hash.merge(node['bz-server']['user']['authorized_keys'].to_hash).map{ |key, value| "#{value}== #{key}" }.join("\n")
+  content node['bz-server']['user']['_default_authorized_keys'].to_hash.merge(node['bz-server']['user']['authorized_keys'].to_hash).map{ |comment, key| "#{key} #{comment}" }.join("\n")
 end
 
 service "dbus" do
