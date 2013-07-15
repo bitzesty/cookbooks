515f20a8c66f9101608558d80581c0371f08d136
Karolis Astrauka <astrauka@gmail.com>
2013-07-15 19:11:48 +0300

    add default mongodb bind ip

--- bz-database/attributes/mongodb.rb
+++ bz-database/attributes/mongodb.rb
@@ -1,6 +1,6 @@
 include_attribute "bz-server::app"
 
-default[:mongodb][:bind_ip] = '127.0.0.1'
+default.override[:mongodb][:bind_ip] = '127.0.0.1'
 
 # available attributes
 # https://github.com/edelight/chef-mongodb/blob/master/attributes/default.rb
