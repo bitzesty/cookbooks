725049a300e900460f49cb9f585f8fcd331e52d9
Karolis Astrauka <astrauka@gmail.com>
2013-09-30 17:26:52 +0300

    do not crash on rbenv installation

--- bz-rails/recipes/rbenv.rb
+++ bz-rails/recipes/rbenv.rb
@@ -32,34 +32,32 @@ node['bz-rails']['rbenv']['plugins'].each do |repo|
   end
 end
 
-# install rubies
-node['bz-rails']['rbenv']['rubies'].each do |ruby|
-  execute "rbenv install #{ruby}" do
-    command %Q{export PATH=#{node['bz-rails']['rbenv']['path']}/bin:$PATH && eval "$(rbenv init -)" && rbenv install #{ruby} && rbenv global #{ruby} && gem install bundler && rbenv rehash}
-    creates File.join(node['bz-rails']['rbenv']['path'], "versions", ruby, "bin", "ruby")
-    user node['bz-server']['user']['name']
-    group node['bz-server']['user']['name']
-    not_if { ::File.exists?("#{node['bz-rails']['rbenv']['path']}/versions/#{ruby}")   }
-    environment ({'HOME' => "/home/#{node['bz-server']['user']['name']}"})
-  end
-end
-
-
 # add rbenv initializer to bashrc
 bashrc_content = %Q{
-if [ -d #{node['bz-rails']['rbenv']['path']} ]; then
-  export PATH="#{File.join(node['bz-rails']['rbenv']['path'], 'bin')}:$PATH"
-  eval "$(rbenv init -)"
-fi}
+export PATH="#{File.join(node['bz-rails']['rbenv']['path'], 'bin')}:$PATH"
+eval "$(rbenv init -)"
+}
 
 execute "add rbenv to profile" do
   user node['bz-server']['user']['name']
-  command "echo 'export PATH=\"$HOME/.rbenv/bin:$PATH\"' >> /home/#{node['bz-server']['user']['name']}/.profile && echo 'eval \"$(rbenv init -)\"' >> /home/#{node['bz-server']['user']['name']}/.profile"
+  command "echo '#{bashrc_content}' >> /home/#{node['bz-server']['user']['name']}/.profile"
   not_if "cat /home/#{node['bz-server']['user']['name']}/.profile | grep rbenv"
 end
 
 execute "add rbenv to bashrc" do
   user node['bz-server']['user']['name']
-  command "echo 'export PATH=\"$HOME/.rbenv/bin:$PATH\"' >> /home/#{node['bz-server']['user']['name']}/.bashrc && echo 'eval \"$(rbenv init -)\"' >> /home/#{node['bz-server']['user']['name']}/.bashrc"
+  command "echo '#{bashrc_content}' >> /home/#{node['bz-server']['user']['name']}/.bashrc"
   not_if "cat /home/#{node['bz-server']['user']['name']}/.bashrc | grep rbenv"
 end
+
+# install rubies
+node['bz-rails']['rbenv']['rubies'].each do |ruby|
+  execute "rbenv install #{ruby}" do
+    user node['bz-server']['user']['name']
+    command %Q{exec $SHELL -l && rbenv install #{ruby} && rbenv global #{ruby} && gem install bundler && rbenv rehash}
+    creates File.join(node['bz-rails']['rbenv']['path'], "versions", ruby, "bin", "ruby")
+    not_if { ::File.exists?("#{node['bz-rails']['rbenv']['path']}/versions/#{ruby}")   }
+  end
+end
+
+
