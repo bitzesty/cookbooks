de55dc513edd19e475d0409f4aa2d6bb754dcb30
Karolis Astrauka <astrauka@gmail.com>
2013-10-24 18:32:48 +0300

    add imagemagick to default installation

--- bz-database/metadata.rb
+++ bz-database/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'info@bitzesty.com'
 license          'All rights reserved'
 description      'Database configurationr recipe'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.8'
+version          '0.1.9'
 
 depends          'database'
 depends          'mongodb'
