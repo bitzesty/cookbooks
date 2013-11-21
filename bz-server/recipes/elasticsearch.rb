1372624e7171395f8d23ca55923317733a4fef88
Karolis Astrauka <astrauka@gmail.com>
2013-11-21 11:46:55 +0200

    explain what elastic monit is doing

--- bz-server/recipes/elasticsearch.rb
+++ bz-server/recipes/elasticsearch.rb
@@ -1,4 +1,6 @@
 include_recipe "java"
 include_recipe "monit"
 include_recipe "elasticsearch::default"
-include_recipe "elasticsearch::monit" # monitoring
+# Check whether elasticsearch is running, reachable by HTTP and
+# the cluster is in the "green" state
+include_recipe "elasticsearch::monit"
