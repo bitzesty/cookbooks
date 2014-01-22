ac15e1536a3303b23b0517bfebc0c20252936cf0
RuslanHamidullin <ror_developer@mail.ru>
2014-01-22 17:57:08 +0400

    [ Nginx ] default site error fix

--- bz-webserver/recipes/nginx.rb
+++ bz-webserver/recipes/nginx.rb
@@ -2,6 +2,10 @@ include_recipe "bz-webserver::common"
 
 include_recipe "nginx"
 
+file "/etc/nginx/sites-enabled/default" do
+  action :delete
+end
+
 template "/etc/nginx/sites-enabled/#{node['bz-server']['app']['name']}-vhost" do
   source "nginx-vhost.erb"
   notifies :reload, "service[nginx]", :immediately
