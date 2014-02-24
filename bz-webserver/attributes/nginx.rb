18fc4f04a802e3e97840f5f809e30c7855b514e3
Karolis Astrauka <astrauka@gmail.com>
2014-02-24 14:17:15 +0200

    update nginx configuration with ssl support and maintenance page

--- bz-webserver/attributes/nginx.rb
+++ bz-webserver/attributes/nginx.rb
@@ -3,8 +3,13 @@ default['bz-webserver']['open-80-port'] = true
 
 bz_nginx_opts = default['bz-webserver']['nginx']
 
+bz_nginx_opts['use_ssl'] = false
+bz_nginx_opts['certs']['dir'] = "#{node['bz-server']['app']['path']}/certs"
+bz_nginx_opts['certs']['certificate'] = "#{bz_nginx_opts['certs']['dir']}/#{node['bz-server']['app']['name']}.crt"
+bz_nginx_opts['certs']['key'] = "#{bz_nginx_opts['certs']['dir']}/#{node['bz-server']['app']['name']}.key"
+
 bz_nginx_opts['worker_processes'] = "1"
-bz_nginx_opts['log_dir'] = "#{node['bz-server']['user']['home']}/nginx/log"
-bz_nginx_opts['error_log'] = "#{bz_nginx_opts['log_dir']}/error.log"
-bz_nginx_opts['access_log'] = "#{bz_nginx_opts['log_dir']}/access.log"
+bz_nginx_opts['log_dir'] = "#{node['bz-rails']['shared_path']}/log"
+bz_nginx_opts['error_log'] = "#{bz_nginx_opts['log_dir']}/nginx.error.log"
+bz_nginx_opts['access_log'] = "#{bz_nginx_opts['log_dir']}/nginx.access.log"
 bz_nginx_opts['pid'] = "/var/run/nginx.pid" # in order to change it need to update nginx service conf
