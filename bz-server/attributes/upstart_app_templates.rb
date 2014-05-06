37964abb2b407e13b3558a1efdfcf7dad8af1805
Karolis Astrauka <astrauka@gmail.com>
2014-05-06 15:05:28 +0300

    allow disabling upstart templates

--- bz-server/attributes/upstart_app_templates.rb
+++ bz-server/attributes/upstart_app_templates.rb
@@ -2,3 +2,4 @@ include_attribute "bz-rails::default"
 
 upstart_templates = default['bz-server']['upstart_templates']
 upstart_templates['path'] = File.join(node['bz-rails']['shared_path'], "config", "deploy", "templates")
+upstart_templates['enabled'] = true # enabled by default
--- bz-server/recipes/default.rb
+++ bz-server/recipes/default.rb
@@ -11,7 +11,9 @@ include_recipe "bz-server::setup"
 include_recipe "bz-server::locales"
 include_recipe "bz-server::user"
 include_recipe "bz-server::upstart"
-include_recipe "bz-server::upstart_app_templates"
+if node['bz-server']['upstart_templates']['enabled']
+  include_recipe "bz-server::upstart_app_templates"
+end
 include_recipe "bz-server::openssh"
 include_recipe "bz-server::logrotate"
 # only include if we are monitoring via serverdensity
