5f10c384d99b26e08f37c76dad597df2746a500c
Karolis Astrauka <astrauka@gmail.com>
2014-05-09 11:10:18 +0300

    safe name for varnish storage file

--- bz-webserver/attributes/varnish.rb
+++ bz-webserver/attributes/varnish.rb
@@ -1,5 +1,7 @@
 bz_varnish = default['bz-webserver']['varnish']
 bz_varnish['listen_port'] = 80 # listen on 80 by default, nginx should go on 8080 then
+# safe name for file location
+bz_varnish['storage_file'] = "/var/lib/varnish/#{node['bz-server']['user']['name'].gsub(/-/, "_")}/varnish_storage.bin"
 
 # forward attributes to node['varnish']
 default['varnish'] = default['varnish'].merge(node['bz-webserver']['varnish'])
