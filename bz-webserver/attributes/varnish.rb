06ae1fe84f24343a513c252d584f40c62da8c3e6
Karolis Astrauka <astrauka@gmail.com>
2014-05-09 12:11:48 +0300

    define varnish user dir

--- bz-webserver/attributes/varnish.rb
+++ bz-webserver/attributes/varnish.rb
@@ -1,7 +1,9 @@
 bz_varnish = default['bz-webserver']['varnish']
 bz_varnish['listen_port'] = 80 # listen on 80 by default, nginx should go on 8080 then
 # safe name for file location
-bz_varnish['storage_file'] = "/var/lib/varnish/#{node['bz-server']['user']['name'].gsub(/-/, "_")}/varnish_storage.bin"
+user_name = node['bz-server']['user']['name'].gsub(/-/, "_")
+bz_varnish['instance'] = user_name
+bz_varnish['storage_file'] = "/var/lib/varnish/#{user_name}/varnish_storage.bin"
 
 # forward attributes to node['varnish']
 default['varnish'] = default['varnish'].merge(node['bz-webserver']['varnish'])
