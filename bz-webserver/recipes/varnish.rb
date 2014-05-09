60540e03cc48b9634efaa2c9d76be5a40b1407b8
Karolis Astrauka <astrauka@gmail.com>
2014-05-09 10:02:39 +0300

    restart varnish

--- bz-webserver/recipes/varnish.rb
+++ bz-webserver/recipes/varnish.rb
@@ -4,18 +4,7 @@ include_recipe "varnish::apt_repo"
 # install varnish
 include_recipe "varnish::default"
 
-# open admin port
-port = node['varnish']['admin_listen_port'].to_s
-if platform_family?("rhel")
-  execute "Open firewall port #{port} for RHEL" do
-    command %Q{iptables -I INPUT -p tcp --dport #{port} -j ACCEPT ; service iptables save}
-    not_if { `grep "dport #{port}" /etc/sysconfig/iptables`.include? port }
-  end
-end
-
-if platform_family?("debian")
-  firewall_rule "varnish_admin" do
-    port port.to_i
-    action :allow
-  end
+# restart
+service "varnish" do
+  action :restart
 end
