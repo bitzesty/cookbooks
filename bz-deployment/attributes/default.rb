cf39dfdf4855b97892d9015fe877c484f8a6cefd
RuslanHamidullin <ror_developer@mail.ru>
2014-06-03 11:00:33 +0400

    [BZ:DEPLOYMENT] first steps

---
+++ bz-deployment/attributes/default.rb
@@ -0,0 +1,4 @@
+include_attribute "bz-server::app"
+include_attribute "bz-server::user"
+include_attribute "bz-database::backup"
+include_attribute "bz-rails"
--- bz-deployment/metadata.rb
+++ bz-deployment/metadata.rb
@@ -4,7 +4,10 @@ maintainer_email 'info@bitzesty.com'
 license          'All rights reserved'
 description      'Application development via chef and capistrano recipe'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.31"
+version          "0.1.32"
 
-# list dependencies
-# depends          'database'
+depends "bz-server"
+depends "bz-rails"
+depends "bz-database"
+depends "bz-webserver"
+
