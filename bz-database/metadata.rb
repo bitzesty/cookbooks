e0b2448db01ac79607cf3edb3dd540fac5e180fb
Marcin Olichwirowicz <olichwirowicz@gmail.com>
2014-07-18 16:28:13 +0200

    Update cookbooks

--- bz-database/metadata.rb
+++ bz-database/metadata.rb
@@ -4,9 +4,9 @@ maintainer_email 'info@bitzesty.com'
 license          'All rights reserved'
 description      'Database configurationr recipe'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.3"
+version          "0.2.4"
 
-depends          'database'
-depends          'mongodb'
-depends          'postgresql'
-depends          'redisio'
+depends          'database', '2.2.1'
+depends          'mongodb', '0.16.1'
+depends          'postgresql', '3.4.1'
+depends          'redisio', '1.7.1'
