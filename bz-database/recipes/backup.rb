ada0a45dcd391aee22856543cbedbda1f9e57d11
Matthew Ford <matt@bitzesty.com>
2016-01-24 18:42:43 +0000

    correct host

--- bz-database/recipes/backup.rb
+++ bz-database/recipes/backup.rb
@@ -3,7 +3,7 @@
 log "[Backup] installing backup gem"
 execute "install backup gem" do
   # ensure other backup versions are uninstalled
-  command %Q{export PATH=#{node['bz-rails']['rbenv']['path']}/bin:$PATH && eval "$(rbenv init -)" && gem uninstall -a -x backup && gem install backup -v 3.10.0 && rbenv rehash}
+  command %Q{gem uninstall -a -x backup && gem install backup -v 3.10.0}
   user node['bz-server']['user']['name']
   group node['bz-server']['user']['name']
   environment ({'HOME' => "/home/#{node['bz-server']['user']['name']}"})
